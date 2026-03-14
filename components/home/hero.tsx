d2cabe99e1ea9cd69f767c68df91bc14ee13619f
Pleasant Yomi <110311997+pleasantyomi@users.noreply.github.com>
2026-03-14 17:33:09 +0100

    Update hero.tsx

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Inter } from "next/font/google";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
